86b85151cb2a7b872800c1020c88b5e7bc951cf1
SL8R <sl8erhw@gmail.com>
2019-08-21 15:11:34 -0700

    fortran hello world

--- hello.f90
+++ hello.f90
@@ -1,5 +1,5 @@
 
-program hellotest
-  implicit none
-  write(*,*) 'everybodyyyyy.... rock your body... backstreet's back, alright!'
-end program hellotest
+program hello
+    implicit none
+    write(*,*) 'Hello world'
+  end program hello
