03659403090b514f0f6fe67cc2843f1ff50811c8
kyler.little <kyler.little@wsu.edu>
2019-09-27 21:55:40 -0700

    fix api issue + refactor; add stream stdout/err; can't run it still for some weird reason

--- test/mvp-output/get_output.py
+++ test/mvp-output/get_output.py
@@ -1,7 +1,45 @@
 import docker
 import time
 
-IM_NAME='larairpact/bluesky-framework'
+IM_NAME='larairpact/bluesky-framework'  # image to pull/run
+DATE = '20190901'                       # date running BSF on
+TIME_TO_GET_FROM_CONTAINER = 60         # seconds to allow program to extract data from docker container
+POLLING_FREQUENCY = 5                   # seconds we wait before polling docker container to see if it finished its run
+
+def poll_for_output(container):
+    """Polls container for output
+    """
+    api_client = docker.APIClient()
+    time_waited = 0
+
+    while True:
+        try:
+            # as long as y'all don't recommit the container, directory will be 00.4 for this date
+            # for a different date that doesn't exist in the container, will be 00.1
+            bits, stat = api_client.get_archive(
+                container=container.id,
+                path='/bluesky/dist/bluesky/output/{date}00.4'.format(date=DATE))
+            break
+        except docker.errors.APIError:
+            time.sleep(POLLING_FREQUENCY)
+            time_waited += POLLING_FREQUENCY
+            print("waited {wait_time} seconds for bluesky run to finish".format(wait_time=time_waited))
+            continue
+
+    return bits, stat
+
+
+def stream_std(container):
+    """Streams any output from container to its stdout, stderr
+    """
+    # stream stdout of container
+    for line in container.logs(stream=True):
+        line_string = line.strip().decode("utf-8")
+        print(line_string)
+
+        if 'ERROR' in line_string:
+            raise Exception('Fuck')
+
 
 def main():
     client = docker.from_env()
@@ -10,27 +48,18 @@ def main():
     client.images.pull(IM_NAME)
     
     # run and wait at the end
-    # TODO > don't hardcode the date
     container = client.containers.run(
         IM_NAME,
-        '/bluesky/dist/bluesky/base/lib/bluesky -d 2019090100Z -K no-archive defaultLAR_SFonly && sleep 60',
+        '/bluesky/dist/bluesky/base/lib/bluesky -d {date}00Z -K no-archive defaultLAR_SFonly && sleep {time_to_wait}'
+            .format(date=DATE, time_to_wait=TIME_TO_GET_FROM_CONTAINER),
         detach=True)
 
-    while True:
-        try:
-            # TODO
-            # > this throwing weird error -- fix
-            # > don't hardcode the date
-            bits, stat = docker.APIClient.get_archive(
-                container=container.id,
-                path='/bluesky/dist/bluesky/output/2019090100.4')
-            break
-        except docker.errors.APIError:
-            time.sleep(5)
-            print("waiting for bluesky run to finish")
-            continue
+    # stream the output for debugging purposes
+    stream_std(container)
 
-    print("stats: ", stat)
+    # continously poll the container for output until we get it
+    dirbits, dirstats = poll_for_output(container)
+    print("stats: ", dirstats)
 
     # TODO > write bits to file for Garrett to access
 
